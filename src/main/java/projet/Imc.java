c79fe685c8f133cbd2dcc9b11af8cc3ee71e8bed
hicham <hicham.dizay199@gmail.com>
2025-03-20 03:52:14 +0100

    Premier commit

---
+++ src/main/java/projet/Imc.java
@@ -0,0 +1,21 @@
+package projet;
+
+public class Imc {
+	private double taille;
+    private double poids;
+
+
+    public Imc(double taille, double poids) {
+        this.taille = taille;
+        this.poids = poids;
+    }
+
+  
+    public double calcul() {
+        if (taille > 0) {
+            return poids / (taille * taille);
+        }
+        return 0; 
+    }
+
+}
